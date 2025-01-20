cfb7d0bbef4940cfa45a894239d613318d33d0f9
Cesar Zegarra <czegarram@gmail.com>
2025-01-20 01:09:42 -0300

    feat: updating to dockerfile

---
+++ docker/production/gunicorn_start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+BASE_NAME=probaar
+BASE_DIR=/srv
+PROJECTDIR=$BASE_DIR/src
+SOCKFILE=$BASE_DIR/run/gunicorn.sock
+LOGS_DIR=/var/log/probaar
+USER=probaar
+GROUP=probaar
+NUM_WORKERS=3
+DJANGO_WSGI_MODULE=probaar.wsgi
+
+cd $PROJECTDIR
+
+RUNDIR=$(dirname $SOCKFILE)
+[ -d $RUNDIR ] || mkdir -p $RUNDIR
+
+gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $BASE_NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-level=info \
+  --bind 0.0.0.0:8000 \
+  --log-file=$LOGS_DIR/gunicorn.log \
+  --access-logfile=$LOGS_DIR/gunicorn.access.log \
+  --timeout 60 \
+  --config=${PROJECTDIR}/${BASE_NAME}/gunicorn-config.py
+
